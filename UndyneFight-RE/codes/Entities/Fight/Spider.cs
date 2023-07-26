c42c804e393ffb06d7a9ccccd43a49c264289bfa
zKron0 <102871688+zKron0@users.noreply.github.com>
2023-07-26 21:56:06 +0800

    upd

---
+++ UndyneFight-RE/codes/Entities/Fight/Spider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using System.Text;
+using System.Threading.Tasks;
+using UndyneFight_Ex.GameInterface;
+using UndyneFight_Ex.Entities;
+using Microsoft.Xna.Framework.Graphics;
+using UndyneFight_Ex.Fight;
+using System.Runtime.ExceptionServices;
+
+namespace UndyneFight_Ex.Remake.Entities
+{
+    public class Spider : Barrage
+    {
+        public Spider(EaseUnit<Vector2> _positionRoute) : this() {
+            this.PositionRoute = _positionRoute.Easing;
+        }
+        public Spider(Func<ICustomMotion, Vector2> _positionRoute) : this() {
+            this.PositionRoute = _positionRoute;
+        }
+        public bool AutoDispose { set;  get; } = true;
+        Spider()
+        {
+            this.Image = Resources.FightSprites.Spider;
+            this.Depth = 0.5f;
+            this._autoDispose = true;
+            this.Alpha = 1.0f;
+            this.UpdateIn120 = true;
+            this.HitRadius = 4f;
+        }
+        protected override float GetDistance(Player.Heart heart)
+        {
+            return Vector2.Distance(heart.Centre, this.Centre);
+        }
+        public override void Start()
+        {
+            _autoDispose = AutoDispose;
+            this.HitRadius = 4f*Scale;
+        }
+    }
+}
