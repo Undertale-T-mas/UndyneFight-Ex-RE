44f12240271b1596fb1ad78315d2dd7d5a3bb270
EdensStudio <83062244+EdensStudio@users.noreply.github.com>
2023-10-08 19:17:14 +0800

    Removed some unused lines Added selection for arrow sound in AudioSetting

--- UndyneFight-RE/codes/Entities/UI/Main/Setting/AudioSetting.cs
+++ UndyneFight-RE/codes/Entities/UI/Main/Setting/AudioSetting.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Media;
 using System;
 using UndyneFight_Ex.Settings;
 using static UndyneFight_Ex.Settings.SettingsManager;
+using static UndyneFight_Ex.Settings.SettingsManager.DataLibrary;
 
 namespace UndyneFight_Ex.Remake.UI
 {
@@ -16,9 +17,13 @@ namespace UndyneFight_Ex.Remake.UI
                 public AudioSetting() : base("Audio", 135)
                 {
                     this.AddChild(_masterVolume = new ScrollBar(new CollideRect(510 - 80, 80, 80 * 2, 60), "Master Volume", 0, 100, this)
-                    { DefaultValue = DataLibrary.masterVolume });
+                    { DefaultValue = masterVolume });
                     this.AddChild(_spearVolume = new ScrollBar(new CollideRect(510 - 80, 180, 80 * 2, 60), "Spear Volume", 0, 100, this)
-                    { DefaultValue = DataLibrary.SpearBlockingVolume });
+                    { DefaultValue = SpearBlockingVolume });
+                    AddChild(_spearBlockSound = new AlternateButton(this, new(510, 130), "Arrow Sound", "Classic", "New")
+                    {
+                        DefaultValue = SpearBlockSound.ToString()
+                    });
                     _masterVolume.OnChange += () => { 
                         _masterVolume.Volume = _masterVolume.GetValue() / 100f;
                         _spearVolume.Volume = _masterVolume.GetValue() / 100f * _spearVolume.GetValue() / 100f;
@@ -29,18 +34,26 @@ namespace UndyneFight_Ex.Remake.UI
                     _spearVolume.ChangeSound = FightResources.Sounds.ArrowStuck;
 
                     this.OnActivated += () => {
-                        _masterVolume.SetValue(DataLibrary.masterVolume);
-                        _spearVolume.SetValue(DataLibrary.SpearBlockingVolume);
+                        _spearBlockSound.DefaultValue = SpearBlockSound.ToString();
+                        _masterVolume.SetValue(masterVolume);
+                        _spearVolume.SetValue(SpearBlockingVolume);
                     };
                 }
                 ScrollBar _masterVolume, _spearVolume;
+                AlternateButton _spearBlockSound;
 
                 public override void Apply()
                 {
-                    DataLibrary.masterVolume = (int)MathF.Round(_masterVolume.GetValue(), 0);
-                    DataLibrary.SpearBlockingVolume = (int)MathF.Round(_spearVolume.GetValue(), 0);
+                    SpearBlockSound = _spearBlockSound.Result switch
+                    {
+                        "Classic" => 1,
+                        "New" => 0,
+                        _ => throw new Exception()
+                    };
+                    masterVolume = (int)MathF.Round(_masterVolume.GetValue(), 0);
+                    SpearBlockingVolume = (int)MathF.Round(_spearVolume.GetValue(), 0);
 
-                    MediaPlayer.Volume = SoundEffect.MasterVolume = MathF.Pow(DataLibrary.masterVolume / 100f, 2);
+                    MediaPlayer.Volume = SoundEffect.MasterVolume = MathF.Pow(masterVolume / 100f, 2);
                 }
                 public override void Update()
                 { 
