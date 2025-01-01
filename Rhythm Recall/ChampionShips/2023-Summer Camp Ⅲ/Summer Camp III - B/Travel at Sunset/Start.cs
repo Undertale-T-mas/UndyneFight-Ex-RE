83f07cfd909450cb3818824a87a639ac9aad0eba
meeeetototototototo <tk.pppo@gmail.com>
2025-01-02 08:39:46 +0900

    Revert "a"
    This reverts commit 45e0c892ee8e0d8fc0422087881ea4e3b98e95a0.

--- Rhythm Recall/ChampionShips/2023-Summer Camp Ⅲ/Summer Camp III - B/Travel at Sunset/Start.cs
+++ Rhythm Recall/ChampionShips/2023-Summer Camp Ⅲ/Summer Camp III - B/Travel at Sunset/Start.cs
@@ -151,12 +151,11 @@ namespace Rhythm_Recall.Waves
                 ScreenDrawing.ScreenScale = 2f;
                 CreateEntity(sans = new Sans(Loader));
                 bool jump = false;
-                FightBox.instance.AlphaBlend = false;
                 if (GameStates.difficulty == 0) jump = false;
                 if (jump)
                 {
                     float beat;
-                    beat = 926;
+                    beat = 1020;
                     sans.Alpha = 0.0f;
                     GametimeDelta += BeatTime(beat);
 
